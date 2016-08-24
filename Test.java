10b52b4179bde86a8e88beb8903dd433a752fc10
reachsaurabh <reachsaurabh@users.noreply.github.com>
2016-08-24 21:44:25 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,13 +1,10 @@
 package uk.gov.moj.cpp.outcomes.domain.event;
 
-import org.apache.commons.lang3.ObjectUtils;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
-import java.util.function.Supplier;
 
 /**
  * Created by saurabhkumar on 13/08/2016.
@@ -43,7 +40,7 @@ public class Test {
             "\t\"id\": \"0001\",\n" +
             "\t\"type\": \"donut\",\n" +
             "\t\"name\": \"Cake\",\n" +
-            "\t\"ppu\": 0.55,\n" +
+            "\t\"ppu\": 0.51,\n" +
             "\t\"batters\":\n" +
             "\t\t{\n" +
             "\t\t\t\"batter\":\n" +
@@ -62,51 +59,15 @@ public class Test {
             "\t\t\t{ \"id\": \"5007\", \"type\": \"Powdered Sugar\" },\n" +
             "\t\t\t{ \"id\": \"5006\", \"type\": \"Chocolate with Sprinkles\" },\n" +
             "\t\t\t{ \"id\": \"5003\", \"type\": \"Chocolate\" },\n" +
-            "\t\t\t{ \"id\": \"5004\", \"type\": \"Maple\" }\n" +
+           // "\t\t\t{ \"id\": \"5004\", \"type\": \"Maple\" }\n" +
             "\t\t]\n" +
             "}");
 
-    private static ThreadLocal<String> valueExtracted = new ThreadLocal<>();
-
-
-
-    public static void main(String... s) {
-        List<String> ignoreList = new ArrayList<String>();
-        HearingEventAdded hearingEventAdded = new HearingEventAdded();
-        Wrapper tt = new Wrapper();
-        //tt.setTest("ss");
-        //hearingEventAdded.setTest(tt);
-        Wrapper tt1 = new Wrapper();
-        System.out.println("tt1" + tt1.getTest());
-        tt.setTest(null);
-        hearingEventAdded.setTest(tt);
-        extractValue(()->hearingEventAdded.getTest().getTest()).ifPresent(s1 -> tt1.setTest(s1));
-
-
-
-        ///optionalTypeDirName.ifPresent(typeDirName -> System.out.println(typeDirName));
-
-        // boolean check = Objects.requireNonNull(hearingEventAdded.getTest(),"");
-
-        //ignoreList.add("batter");
-        //boolean succuss = JSONUtils.areJSONEqual(jsonObject1,jsonObject2,ignoreList);
-        System.out.println("tt1" + tt1.getTest());
-    }
-
-    public static <T> Optional<T> extractValue(Supplier<T> resolver ){
-        try {
-            T result = resolver.get();
-            return Optional.ofNullable(result);
-        } catch (NullPointerException e) {
-            return Optional.empty();
-        }
-    }
-
-    static <T> void getValue(Optional<T> value)
+    public static void main(String ... s)
     {
-        if(value.isPresent())
-            value.get();
+        List<String> ignoreList = new ArrayList<String>();
 
+        boolean check = JSONUtils.areJSONEqual(jsonObject1,jsonObject2,ignoreList);
+        System.out.println("JSON Objects are Equal " + check);
     }
-
 }
